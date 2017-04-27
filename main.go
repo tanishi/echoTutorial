410b8314e0b400c6b553edb90a1a2c91de48301c
tanishi <tanishi.success@gmail.com>
2017-04-27 23:41:10 +0900

    modified:   main.go

--- main.go
+++ main.go
@@ -4,14 +4,39 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"net/http"
+	"strconv"
 )
+
+type (
+	task struct {
+		id    int    `json:"id"`
+		title string `json:"title"`
+	}
+)
+
+var (
+	todo = map[int]*task{}
+	seq  = 1
+)
+
+func getTask(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, *todo[id])
+}
 
 func main() {
 	e := echo.New()
 
+	todo[0] = &task{
+		id:    0,
+		title: "ichigo",
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/todo/:id", getTask)
 
 	e.Run(standard.New(":9999"))
 }
